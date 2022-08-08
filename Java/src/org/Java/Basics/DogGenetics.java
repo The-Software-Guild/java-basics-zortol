24899e7559ca6137b76ce6e32a4e29a6650678af
zortol <nepal_14221@hotmail.com>
2022-08-08 16:39:39 +0100

    Implemented the DogGenetics function

---
+++ Java/src/org/Java/Basics/DogGenetics.java
@@ -0,0 +1,34 @@
+package org.Java.Basics;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DogGenetics {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println("What is your dog's name?");
+        String dogName = scan.next();
+
+        System.out.println("Well then, I have this highly reliable report on " + dogName + "'s prestigious background right here.");
+
+        System.out.println(dogName + " is:");
+
+        int[] breedPercent = new int[5];
+        int numberToDec = 100;
+        for(int i =0; i < breedPercent.length; i++){
+            int in = rand.nextInt(numberToDec) + 1;
+            breedPercent[i] = in;
+            numberToDec -= in;
+        }
+        System.out.println(breedPercent[0] + "% St. Bernard");
+        System.out.println(breedPercent[1] + "% Chihuahua");
+        System.out.println(breedPercent[2] + "% Dramatic RedNosed Asian Pug");
+        System.out.println(breedPercent[3] + "% Common Cur");
+        System.out.println(breedPercent[4] + "% King Doberman");
+
+        System.out.println("Wow, that's QUITE the dog!");
+    }
+}

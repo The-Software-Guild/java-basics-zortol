a64798403c3f36635d1efad390e6cf3a4e97478b
zortol <nepal_14221@hotmail.com>
2022-08-09 10:06:01 +0100

    Changed the SummativeSums function's for loop

--- Java/src/org/Java/Basics/SummativeSums.java
+++ Java/src/org/Java/Basics/SummativeSums.java
@@ -5,9 +5,9 @@ public class SummativeSums {
     public static int arrSum(int[] arr) {
         int total = 0;
 
-        for(int i =0; i < arr.length;i++){
-            total += arr[i];
-        }
+        for (int x : arr)
+            total += x;
+
 
         return total;
     }
